539d6a89f30614e38971a92a25cf70752ed5f282
jiaccool <jiaccool@163.com>
2018-10-12 08:26:03 +0800

    no message

--- src/pages/accept-select/accept-select.module.ts
+++ src/pages/accept-select/accept-select.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { AcceptSelectPage } from './accept-select';
+//指令
+import { TabAcceptSelectDirective } from "../../directive/";
 
 @NgModule({
   declarations: [
     AcceptSelectPage,
+    TabAcceptSelectDirective
   ],
   imports: [
     IonicPageModule.forChild(AcceptSelectPage),
